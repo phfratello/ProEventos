2041fc52af51b92d06225ebb7b178eed16f3bfb0
unknown <3con@3CSPP000609N.paulista.3con>
2021-07-14 22:57:25 -0300

    Seção 2: DotNet 5

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+       
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public Evento Get()
+        {  
+            return new Evento(){
+                EventoId = 1,
+                Tema = "Angular 11  e .NET 5",
+                Local = "Belo Horizonte",
+                Lote = "1º Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString(),
+                ImagemURL = "foto.png"
+            };
+        }
+
+        [HttpPost]  // criação
+        public string Post()
+        {  
+            return "Exemplo de Post";
+        }
+
+          [HttpPut("{id}")]  // substitui o objeto inteiro
+        public string Put(int id)
+        {  
+            return $"Exemplo de Put com id = {id}";
+        }
+
+             [HttpDelete("{id}")]  // deletar objetos
+        public string Delete(int id)
+        {  
+            return $"Exemplo de Delete com id = {id}";
+        }
+    }
+}
